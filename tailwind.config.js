fe4cc539042c96e3bcf0fe0e9207651beba4bcc6
Anuj kumar <trigbit.anuj@gmail.com>
2023-06-26 18:22:03 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,26 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-ltr":
+          "linear-gradient(to right, #e6e6e6 5%, #cccccc 25%, #e6e6e6 35%)",
+      },
+
+      backgroundSize: {
+        "x-y": "1000px 100%",
       },
       boxShadow: {
         custom: "1px 2px 4px 1px rgba(0, 0, 0, 0.08)",
         header: " -1px 4px 20px 14px rgba(0, 0, 0, 0.2)",
+        button: "-1px 4px 2px 1px rgba(13,122,205,1)",
+      },
+      animation: {
+        "animate-ltr": "animate 3s ease-out infinite ",
+      },
+      keyframes: {
+        animate: {
+          "0%": { backgroundPosition: "-1000px 0" },
+          "100%": { backgroundPosition: "1000px 0" },
+        },
       },
     },
   },
