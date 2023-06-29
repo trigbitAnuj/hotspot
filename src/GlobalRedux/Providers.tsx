200338a3fbd0578a31bd7f30097b315a7d2db51e
Anuj kumar <trigbit.anuj@gmail.com>
2023-06-29 16:24:42 +0530

    add CheckUserLoginProvider Component

--- src/GlobalRedux/Providers.tsx
+++ src/GlobalRedux/Providers.tsx
@@ -4,17 +4,31 @@ import { store } from "@/GlobalRedux/store";
 import Header from "../app/component/Header";
 import { QueryClientProvider, QueryClient } from "react-query";
 import FooterComponent from "@/app/component/FooterComponent";
+import { UseAuthProvider } from "@/firebase/auth";
+import Login from "@/app/(auth)/login/page";
+import { useRouter } from "next/navigation";
+import { getUserfromLocalStorage } from "@/utils";
+
+export const CheckUserLogInProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const router = useRouter();
+  const { user } = getUserfromLocalStorage();
+  if (!user) {
+    router.push("/login");
+  } else {
+    return children;
+  }
+};
 
 const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   const queryClient = new QueryClient();
 
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Header />
-        {children}
-        <FooterComponent />
-      </QueryClientProvider>
+      <Header />
+      {children}
+      <FooterComponent />
     </Provider>
   );
 };
