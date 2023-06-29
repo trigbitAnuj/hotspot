3bbe342552fd4927e0a94899d2b34662b510e914
Anuj kumar <trigbit.anuj@gmail.com>
2023-06-29 16:23:34 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { CartItem } from "@/GlobalRedux/features";
+import { User } from "firebase/auth";
 
 export function getcartItems(cartItems: CartItem[]) {
   return cartItems.reduce((count, cartItem) => {
@@ -10,4 +11,16 @@ export const getSubTotal = (cart: CartItem[]) => {
   return cart.reduce((sum, { item, quantity }) => {
     return item.card.info.price * quantity + sum;
   }, 0);
+};
+
+export const getUserfromLocalStorage = () => {
+  const user: User = JSON.parse(localStorage.getItem("user") ?? "{}");
+  return user;
+};
+
+export const login = () => {
+  console.log("user logged in");
+};
+export const logout = () => {
+  console.log("user logged out");
 };
