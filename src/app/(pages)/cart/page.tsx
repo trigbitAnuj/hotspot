fd0e8a95ba49afe0b2250015e6aa03d7e04f9b54
Anuj kumar <trigbit.anuj@gmail.com>
2023-06-30 16:31:55 +0530

    Update page.tsx

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 const Cart: NextPage = () => {
   return (
     <CheckUserLogInProvider>
-      <CartComponent />;
+      <CartComponent />
     </CheckUserLogInProvider>
   );
 };
