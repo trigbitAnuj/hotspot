e44e610967669bb1747ac800b081fa2a05a49460
Anuj kumar <trigbit.anuj@gmail.com>
2023-06-30 18:54:49 +0530

    Update Header.tsx

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -10,6 +10,7 @@ import { useSelector } from "react-redux";
 import { GetUserfromLocalStorage, getcartItems } from "@/utils";
 import { useRouter } from "next/navigation";
 import { UseAuthProvider } from "@/firebase/auth";
+import ProfileComponent from "./ProfileComponent";
 
 const Title: React.FC = () => {
   return (
@@ -56,18 +57,6 @@ const Header: React.FC = () => {
     setShowMenu(false);
   };
 
-  const onMenuEnter = () => {
-    if (timerId.current) {
-      clearTimeout(timerId.current);
-    }
-    setShowProfileMenu(true);
-  };
-  const onMenuLeave = () => {
-    timerId.current = setTimeout(() => {
-      setShowProfileMenu(false);
-    }, 500);
-  };
-
   const handleMenuClick = () => {
     setShowMenu(true);
   };
@@ -79,21 +68,7 @@ const Header: React.FC = () => {
     await signOutUser();
     router.push("/login");
   };
-
-  useEffect(() => {
-    const DivRef = dropDownDivRef.current;
-    if (DivRef) {
-      DivRef.addEventListener("mouseenter", onMenuEnter);
-      DivRef.addEventListener("mouseleave", onMenuLeave);
-    }
-
-    return () => {
-      if (DivRef) {
-        DivRef.removeEventListener("mouseenter", onMenuEnter);
-        DivRef.removeEventListener("mouseleave", onMenuLeave);
-      }
-    };
-  });
+  console.log(user);
 
   return (
     <>
@@ -129,71 +104,7 @@ const Header: React.FC = () => {
               </Link>
             </li>
 
-            {user ? (
-              <>
-                <div ref={dropDownDivRef} className="flex ">
-                  <button
-                    id="dropdownDefaultButton"
-                    data-dropdown-toggle="dropdown"
-                    className="text-white  font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center relative"
-                    type="button"
-                  >
-                    {user?.displayName}
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      aria-hidden="true"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M19 9l-7 7-7-7"
-                      ></path>
-                    </svg>
-                  </button>
-                  {showProfileMenu ? (
-                    <div
-                      id="dropdown"
-                      className="z-10  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute top-20 right-3"
-                    >
-                      <ul
-                        className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                        aria-labelledby="dropdownDefaultButton"
-                      >
-                        <li>
-                          <button className="block px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                            Settings
-                          </button>
-                        </li>
-                        <hr />
-
-                        <li>
-                          <button
-                            onClick={handleSignOut}
-                            className="block px-4 py-2 hover:bg-gray-100 w-full dark:hover:bg-gray-600 dark:hover:text-white"
-                          >
-                            Sign out
-                          </button>
-                        </li>
-                      </ul>
-                    </div>
-                  ) : null}
-                </div>
-              </>
-            ) : (
-              <div>
-                <button
-                  onClick={handleLogin}
-                  className="md:px-3 md:py-2 md:text-base border lg:px-3 lg:py-2 lg:text-lg border-red-500   rounded-md shadow-md shadow-red-500/20 hover:bg-red-600 sm:px-2 sm:py-2 sm:text-sm xs:px-1 xs:py-1 xs:text-xs "
-                >
-                  Login
-                </button>
-              </div>
-            )}
+            {user && <ProfileComponent />}
           </ul>
         </section>
       </header>
