83268bb807f67c03d510045136a69610852b9172
Anuj kumar <trigbit.anuj@gmail.com>
2023-07-06 15:38:21 +0530

    Update Providers.tsx

--- src/GlobalRedux/Providers.tsx
+++ src/GlobalRedux/Providers.tsx
@@ -4,24 +4,7 @@ import { store } from "@/GlobalRedux/store";
 import Header from "@/components/Header";
 import { QueryClientProvider, QueryClient } from "react-query";
 import FooterComponent from "@/components/FooterComponent";
-
-import { useRouter } from "next/navigation";
-import { GetUserfromLocalStorage } from "@/utils";
-
-export const CheckUserLogInProvider: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => {
-  const router = useRouter();
-  const user = GetUserfromLocalStorage();
-  if (user) {
-    if (user.email) {
-      return children;
-    } else {
-      router.push("/login");
-      return <h1>Loading...</h1>;
-    }
-  }
-};
+import { CheckUserLogInProvider } from "@/Helpers/CheckUserLoginProvider";
 
 const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   const queryClient = new QueryClient();
